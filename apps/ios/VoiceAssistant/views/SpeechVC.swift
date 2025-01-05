74c56f536ff74be63caab47458abfbac510c514d
David Wu <KyosukeNo1@gmail.com>
2025-01-04 23:42:11 -0800

    New UI elements

--- apps/ios/VoiceAssistant/views/SpeechVC.swift
+++ apps/ios/VoiceAssistant/views/SpeechVC.swift
@@ -3,9 +3,39 @@ import AVFoundation
 import MicrosoftCognitiveServicesSpeech
 
 
+let BUTTON_ICON_SIZE = 40.0
+
 class SpeechVC: UIViewController {
-    
-    @IBAction func onPhotoActionButtonClicked(_ sender: Any) {
+    @IBOutlet weak var statusImageView: UIImageView!
+
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var attachmentButton: UIButton!
+    @IBOutlet weak var settingsButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self._setupButtons()
+    }
+
+    @IBAction func onStartStopButtonClicked(_ sender: Any) {
     }
     
+    @IBAction func onResetButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func onAttachmentButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func onSettingsuttonClicked(_ sender: Any) {
+    }
+    
+    private func _setupButtons() {
+        if let imageView = self.startStopButton.imageView {
+            imageView.image = UIImage(named: "IconStart")
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.widthAnchor.constraint(equalToConstant: BUTTON_ICON_SIZE).isActive = true
+            imageView.heightAnchor.constraint(equalToConstant: BUTTON_ICON_SIZE).isActive = true
+        }
+    }
 }
