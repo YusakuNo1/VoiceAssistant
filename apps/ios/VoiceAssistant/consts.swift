cdf6e347bae2f8d244f769393fabd5b16cea2226
Wei Wu <KyosukeNo1@gmail.com>
2025-01-07 23:42:10 -0800

    Housekeeping

--- apps/ios/VoiceAssistant/consts.swift
+++ apps/ios/VoiceAssistant/consts.swift
@@ -1,9 +1,4 @@
-////let API_HOST = "http://127.0.0.1:8000"
-//let API_HOST = "http://192.168.254.106:8000"
-////let API_HOST = "http://10.16.176.13:8000"
-
-let API_HOST = "http://192.168.254.107"
-//let API_HOST = "http://10.16.176.13"
+let API_HOST = "http://192.168.254.109"
 
 enum AudioMode {
     case Record
--- backend/src/server.py
+++ backend/src/server.py
@@ -2,7 +2,6 @@ from typing import Annotated
 from fastapi import FastAPI, Header
 
 import src.controllers.chat.controller
-import src.controllers.config
 import src.controllers.speech.controller
 from src.config.app import app
 
@@ -10,8 +9,7 @@ from src.config.app import app
 # Sample query:
 #  curl http://127.0.0.1:8000/test --header "Content-Type: application/json" --header "x-token: my-x-token"
 @app.get("/test")
-async def test(x_token: Annotated[str | None, Header()] = None):
+async def test():
     return {
         "testing": True,
-        "x_token": x_token,
     }
